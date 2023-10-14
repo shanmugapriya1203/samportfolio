a6508059f1aff0a8160dc7a9883f4dea46d1aa28
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-14 22:39:00 +0530

    update about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,37 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React from 'react'
 import developer from '../assets/img/female developer.svg'
-import '../styles/About.css';
+import { Col, Container, Row } from 'react-bootstrap';
+import '../styles/About.css'
 
 function About() {
-    const [h2Text, setH2Text] = useState('');
-    const [isTypingH2, setIsTypingH2] = useState(true);
+  return (
 
-    const h2InitialText = ' About Me';
-
-    useEffect(() => {
-        if (isTypingH2) {
-            const typingInterval = setInterval(() => {
-                if (h2Text.length < h2InitialText.length) {
-                    setH2Text(h2InitialText.slice(0, h2Text.length + 1));
-                } else {
-                    setIsTypingH2(false);
-                }
-            }, 100);
-
-            return () => clearInterval(typingInterval);
-        }
-    }, [isTypingH2, h2Text, h2InitialText]);
-
-    return (
-        <section className='about' id='about'>
+      <section className='about' id='about'>
             <Container>
                 <Row className='align-items-center'>
                     <Col xs={12} md={6} xl={5}>
                         <img src={developer} alt='About Img' />
                     </Col>
                     <Col xs={12} md={6} xl={7}>
-                        <h2>{h2Text}</h2>
+                        <h2> About Me</h2>
                         <p>
                             Passionate MERN Stack Developer with an Electronics and Communication Engineering background. Driven by the art of crafting immersive web experiences, I embarked on the path of website development. With an eye for detail and a love for clean code, I bring creativity and functionality together to build seamless digital solutions. Let's transform ideas into captivating websites that leave a lasting impression.
                         </p>
@@ -39,7 +21,17 @@ function About() {
                 </Row>
             </Container>
         </section>
+
     );
 }
 
-export default About;
+
+
+
+
+
+
+
+
+
+export default About
