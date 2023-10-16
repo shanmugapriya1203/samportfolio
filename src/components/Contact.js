c003514f0e44add02301f668aad3de3b4df46075
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-16 12:25:04 +0530

    change render url

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ const Contact = () => {
 const handleSubmit= async(e)=>{
 e.preventDefault()
 setButtonText('Sending...');
-let response= await fetch('http://localhost:5000/contact',{
+let response= await fetch('https://portfolio-kfok.onrender.com/contact',{
     method:'POST',
     headers:{
         "Content-Type":"Application/json;charset=utf-8",
