b651ce9a2f9062967245d291e83bcaffdd736c22
shanmugapriya1203 <priyamuthukumar1203@gmail.com>
2023-10-16 20:14:06 +0530

    change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import '../styles/Navbar.css';
 import nav1 from '../assets/img/nav1.svg';
 import nav2 from '../assets/img/nav2.svg';
-import nav3 from '../assets/img/nav3.png'
-
+import nav3 from '../assets/img/nav3.png';
 
 function NavigationBar() {
   const [activeLink, setActiveLink] = useState('home');
-  const [scrolled, setScrolled] = useState(false);
-  const [theme, setTheme] = useState('dark');
-
-  useEffect(() => {
-    const onScroll = () => {
-      if (window.scrollY > 50) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-    window.addEventListener('scroll', onScroll);
-    return () => window.removeEventListener('scroll', onScroll);
-  }, []);
 
   const onUpdateActiveLink = (value) => {
     setActiveLink(value);
   };
 
-
   return (
     <>
-      <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
+      <Navbar expand="lg">
         <Container>
           <Navbar.Brand href="#home">
             <h3 className="logo">SAM</h3>
@@ -63,7 +47,6 @@ function NavigationBar() {
               >
                 Projects
               </Nav.Link>
-          
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
@@ -76,16 +59,12 @@ function NavigationBar() {
                 <a href="https://leetcode.com/_sam1203/ " target='_blank'>
                   <img src={nav3} alt="" />
                 </a>
-
-             
               </div>
               <a href="#contact">
-  <button className="vvd">
-    <span>Contact</span>
-  </button>
-</a>
-
-             
+                <button className="vvd">
+                  <span>Contact</span>
+                </button>
+              </a>
             </span>
           </Navbar.Collapse>
         </Container>
